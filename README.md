d48626dbe47e717e849990118049a31afd863f6d
EMPettaway <EMPettaway@gmail.com>
2022-05-07 09:30:07 -0400

    third commit Project1

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # Project1
-SEIR 322 - Project 1 (Card Matching Game)
+** SEIR 322 - Project 1 "Matches" (Card Memory Game)
 
-Questions I was left with...
+An Emoji themed match game
 
-During my styling I was having trouble centering the game board.  The board centered once I went to put the timer class to the left and im not sure why.
+http://127.0.0.1:5502/
+
+•There are 16 squares
+•Find all 8 pairs in one go in the shortest amount of time
+•Press reset to restart the game
+
+**Screenshots
+
+**Technologies Used
+This project was written in VSCode using HTML, CSS, and JavaScript.
+
+**Problems
+There were certain key elements of my game I was unfortunately unable to figure out how to get working in the alloted time such as cards that arent matches to "flip" back over, the timer to work the way I intended for it to work (and not the work around in this current version) and a way to keep track of how many matches the player has.
+
+**User Stories
+
+MVP Goals
+-As a player, I would like to be able to select any card I want.
+-As a player, I want to know when the timer will begin.
+-As a player, I would like to see how many matches I have as the game goes on (score being kept).
+-As a player, I would like to be able to reset the game after my round is over.
+-As a player, I would like the cards to flip over once clicked.
+-As a player, I would like the cards i've already matched to stay face up.
+
+Stretch Goals
+-As a player, I would like scores to be logged onto a leaderboard.
+-As a player, I would like to select between the theme of cards I am matching.
+-As a player, I would like to have BGM while playing the game.
+-As a player, I would like some type of graphics to play whenever I match.
+-As a player, I would like to here a sound whenever I select a card.
+-As a player, I would like an animation as time expires showing how well I did.
+
+**Wireframes
--- script.js
+++ script.js
@@ -5,21 +5,14 @@ console.log(cardImgs);
 let cardImageArray = Object.values(cardImgs)
 const handleClick = (e) => {
     e.target.querySelector(".game-card-img").style.visibility = "visible"
-    // console.log(e.target.style.visibility)
+
 }
 
 cardImageArray.forEach(card => {
 card.parentElement.addEventListener('click', handleClick)
 })
 
-// console.log(cardImageArray);
-
-
-let cardImgsArray = [...cardImgs];
-let openedCards = [];
-let matchedCards = [];
-
-// function startGame()
+// function to randomize cards
 
 function shuffle(array) {
     let currentIndex = array.length,
@@ -37,123 +30,30 @@ function shuffle(array) {
     return array;
 }
 
-// let startButton = document.getElementsByClassName('start');
-
-// document.querySelector('.start').addEventListener('')
-
-// function startGame() 
-
-
-
 //shuffle cards
 let shuffledImages = shuffle(cardImageArray)
 // }
 
 
 for(i=0; i<shuffledImages.length; i++) {
-    //remove all images from previous games from each card (if any)
-    // gameCards[i].innerHTML = "";
     gameCards[i].appendChild(shuffledImages[i]);
-    
-    // add the shuffled images to each card
     gameCards[i].appendChild(shuffledImages[i]);
     gameCards[i].type = `${shuffledImages[i].alt}`;
-    
-    //remove all extra classes for game play
-    gameCards[i].classList.remove("show", "open", "match", "disabled");
-    gameCards[i].children[0].classList.remove("show-img");
 }
 
+// Source for MyTimer function https://stackoverflow.com/questions/32141035/countdown-timer-using-moment-js-mmss-format
 
+function MyTimer(callback, val) {
+    val = val || 60; 
+    var timer=setInterval(function() { 
+        callback(val);
+        if(val-- <= 0) { 
+            clearInterval(timer); 
+        } 
+    }, 1000);
+}
+new MyTimer(function(val) {
+    var timerMsg = "00:" + (val >= 10 ? val : "0" + val);
+    document.getElementById("timer").textContent = timerMsg; 
+});
 
-//     //listen for events on the cards
-//     for(let i = 0; i < gameCardsArray.length; i++) {
-//         gameCardsArray[i].addEventListener("click", displayCard)
-//     }
-
-//     // function flashCards() {
-//     //     for(i=0; i<gameCards.length; i++) {
-//     //         gameCards[i].children[0].classList.add("show-img")
-//     //     }
-//     //     setTimeout(function(){
-//     //         for(i=0; i<gameCards.length; i++) {
-//     //             gameCards[i].children[0].classList.remove("show-img")
-//     //         }
-//     //     }, 1000)
-//     // }
-    
-//     function displayCard() {
-//         this.children[0].classList.toggle('show-img');
-//         this.classList.toggle("open");
-//         this.classList.toggle("show");
-//         this.classList.toggle("disabled");
-//         cardOpen(this);
-//     }
-    
-//     function cardOpen(card) {
-//         openedCards.push(card);
-//         let len = openedCards.length;
-//         // if(len === 2) {
-//         //     moveCounter();
-//         //     if(openedCards[0].type === openedCards[1].type) {
-//         //         matched();
-//         //     } else {
-//         //         unmatched();
-//         //     }
-//         //
-
-// function cardOpen(card) {
-//     openedCards.push(card);
-//     let len = openedCards.length;
-//     if(len === 2) {
-//         moveCounter();
-//         if(openedCards[0].type === openedCards[1].type) {
-//             matched();
-//         } else {
-//             unmatched();
-//         }
-//     }
-// }
-
-// function matched() {
-//     openedCards[0].classList.add("match");
-//     openedCards[1].classList.add("match");
-//     openedCards[0].classList.remove("show", "open");
-//     openedCards[1].classList.remove("show", "open");
-//     matchedCards.push(openedCards[0]);
-//     matchedCards.push(openedCards[1]);
-//     openedCards = [];
-//     if(matchedCards.length == 16) {
-//         endGame();
-//     }
-// }
-
-// function unmatched() {
-//     openedCards[0].classList.add("unmatched");
-//     openedCards[1].classList.add("unmatched");
-//     disable();
-//     setTimeout(function() {
-//         openedCards[0].classList.remove("show", "open", "unmatched");
-//         openedCards[1].classList.remove("show", "open", "unmatched");
-//         openedCards[0].children[0].classList.remove('show-img');
-//         openedCards[1].children[0].classList.remove('show-img');
-//         enable();
-//         openedCards = [];
-        
-//     }, 1100)
-// }
-
-// function disable() {
-//     gameCardsArray.filter((card, i, gameCardsArray) => {
-//         card.classList.add('disabled');
-//     })
-// }
-
-// function enable() {
-//     gameCardsArray.filter((card, i, gameCardsArray) => {
-//         card.classList.remove('disabled');
-//         for(let i=0; i<matchedCards.length; i++) {
-//             matchedCards[i].classList.add('disabled');
-//         }
-//     })
-// }
